92806015f0a361c4ddfb46799fe9999d8767fd1a
diegoffline <diego.marchetti@fatec.sp.gov.br>
2014-12-22 06:03:08 -0200

    comentario no github tenstando o pull

--- PrimeiroGit/src/br/com/tccfatec/primeirogit/MainActivity.java
+++ PrimeiroGit/src/br/com/tccfatec/primeirogit/MainActivity.java
@@ -7,7 +7,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-/* comentario no github */
+// comentario no github 
+//teste
 
 
 	@Override
