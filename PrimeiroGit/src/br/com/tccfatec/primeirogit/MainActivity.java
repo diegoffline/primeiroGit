9c0fe03300e1a259537e9be0ae416ab7f199a099
diegoffline <diego.marchetti@fatec.sp.gov.br>
2014-12-22 05:46:27 -0200

    alteração pelo github

--- PrimeiroGit/src/br/com/tccfatec/primeirogit/MainActivity.java
+++ PrimeiroGit/src/br/com/tccfatec/primeirogit/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+/* comentario no github */
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
