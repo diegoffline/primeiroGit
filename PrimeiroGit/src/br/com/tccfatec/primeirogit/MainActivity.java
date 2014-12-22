1699664022f6d87408d6312ded7b65df9b795b99
Didi <Didi@dIego>
2014-12-22 05:17:38 -0200

    Mudança na classe MainActivit herdando de Activity e na string.xml mudando o Helo World para Oi Mundo

--- PrimeiroGit/src/br/com/tccfatec/primeirogit/MainActivity.java
+++ PrimeiroGit/src/br/com/tccfatec/primeirogit/MainActivity.java
@@ -1,11 +1,12 @@
 package br.com.tccfatec.primeirogit;
 
 import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
